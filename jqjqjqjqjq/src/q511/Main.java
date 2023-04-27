88601e02f6b519d5b040ab0d4fe68987a8748ea6
Class01 <Class01@DESKTOP-V50GOLG>
2023-04-27 09:35:38 +0900

    511 9014ing

--- jqjqjqjqjq/src/q511/Main.java
+++ jqjqjqjqjq/src/q511/Main.java
@@ -4,10 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = (55 -10);
-		System.out.println("55 - 10 = " + a);
-		int b = (2008 - 1999);
-		System.out.println("2008 - 1999 = " + b);
+//		int a = (55 -10);
+//		System.out.println("55 - 10 = " + a);
+//		int b = (2008 - 1999);
+//		System.out.println("2008 - 1999 = " + b);
+	
+		int a = 55;
+        int b = 10;
+ 
+        System.out.println(a + " - " + b + " = " + (a-b));
+         
+        a = 2008;
+        b = 1999; 
+ 
+        System.out.println(a + " - " + b + " = " + (a-b));
+	
 	}
 
 }
