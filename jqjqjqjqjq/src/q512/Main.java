4039d2f307b575f3e5db5f847b2ee37f53b048ae
Class01 <Class01@DESKTOP-V50GOLG>
2023-04-27 15:03:25 +0900

    512소수점

---
+++ jqjqjqjqjq/src/q512/Main.java
@@ -0,0 +1,17 @@
+package q512;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 49;
+		double b = 0.2683;
+//		System.out.printf("49 * 0.268300 = ");
+//		System.out.printf("%f", a*b);
+	
+		System.out.printf("%d%s%f%s%f", a , " * " , b , " = " ,(a*b));
+	
+
+	}
+
+}
