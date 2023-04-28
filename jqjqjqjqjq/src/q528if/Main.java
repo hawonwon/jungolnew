23a1caacc641d17e0353b69ab2894d28cf8a0a7c
Class01 <Class01@DESKTOP-V50GOLG>
2023-04-28 14:02:19 +0900

    528

---
+++ jqjqjqjqjq/src/q528if/Main.java
@@ -0,0 +1,19 @@
+package q528if;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int in = sc.nextInt();
+		System.out.println(in);
+		if (in < 0) {
+			System.out.println("minus");
+		}
+		sc.close();
+		
+	}
+
+}
