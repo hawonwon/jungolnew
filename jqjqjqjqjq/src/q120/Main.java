ba8f00eeca6468b36e49400707d5de57e7e46196
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-01 10:19:17 +0900

    basic530 120 517

--- jqjqjqjqjq/src/q120/Main.java
+++ jqjqjqjqjq/src/q120/Main.java
@@ -1,4 +1,4 @@
-package q517;
+package q120;
 
 import java.util.Scanner;
 
@@ -7,12 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		double d = sc.nextDouble();
-		double d2 = sc.nextDouble();
-		double d3 = sc.nextDouble();
-		System.out.printf("%.3f\n%.3f\n%.3f\n", d, d2, d3);
+		int i = sc.nextInt();
+		int i2 = sc.nextInt();
 		sc.close();
-				
+		if(i >= i2) {
+			System.out.println(i - i2);
+		}else {
+			System.out.println(i2 - i);
+		}
 	}
 
 }
---
+++ jqjqjqjqjq/src/q530/Main2.java
@@ -0,0 +1,19 @@
+package q530;
+
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		sc.close();
+		if (i>=20) {
+			System.out.println("adult");
+		} else {
+			System.out.println((20 - i)+" years later");
+		}
+	}
+
+}
