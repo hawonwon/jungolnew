474ceeff6560d413b4c740d0104600e45493d618
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-01 17:14:37 +0900

    515 9109

---
+++ jqjqjqjqjq/src/q515/Main.java
@@ -0,0 +1,17 @@
+package q515;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		int i = sc.nextInt();
+		int i2 = sc.nextInt();
+		System.out.println(i +" * " + i2 +" = " + i*i2);
+		System.out.println(i +" / " + i2 +" = " + i/i2);
+	}
+
+}
--- jqjqjqjqjq/src/q9109/Main.java
+++ jqjqjqjqjq/src/q9109/Main.java
@@ -6,14 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println(string); //char next().charAt(0)
+//		Scanner sc = new Scanner(System.in);
+//		String string = sc.nextLine();
+//		System.out.println(string); //char next().charAt(0)
 	
-		while(true) {
-			String string = sc.nextLine();
-			
-		}
+		char i = 65;
+		System.out.println(i);
+		
 	
 	}
 
