485b1da2f1eaea4f5247ecff0f08c585ca0ac9cc
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-01 09:37:59 +0900

    514basicScanner

---
+++ jqjqjqjqjq/src/q514/Main.java
@@ -0,0 +1,16 @@
+package q514;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("height = ");
+		int i = scanner.nextInt();
+		System.out.println("Your height is " + i +"cm.");
+		
+	}
+
+}
