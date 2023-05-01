e1f306dc4bc6ddd77f4d79fe8ab06ea43c458107
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-01 16:05:09 +0900

    9109 basic

---
+++ jqjqjqjqjq/src/q9109/Main.java
@@ -0,0 +1,20 @@
+package q9109;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(string); //char next().charAt(0)
+	
+		while(true) {
+			String string = sc.nextLine();
+			
+		}
+	
+	}
+
+}
