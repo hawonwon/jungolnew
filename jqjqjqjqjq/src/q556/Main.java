58f44f5b414206600f90f0ec9121d310be7f9b16
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-02 17:03:42 +0900

    556반복문출력

---
+++ jqjqjqjqjq/src/q556/Main.java
@@ -0,0 +1,17 @@
+package q556;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] i = new int[10];
+//		int i[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		for (int n = 0; n < 10; n++) {
+			i[n] = n + 1;
+			System.out.printf("%d ", i[n]);
+
+		}
+	}
+
+}
