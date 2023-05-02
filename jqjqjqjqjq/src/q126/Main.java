6d1a23305e632156c68ef34da67fb5b9a68736a2
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-02 14:10:28 +0900

    126홀수짝수갯수맞히기

---
+++ jqjqjqjqjq/src/q126/Main.java
@@ -0,0 +1,28 @@
+package q126;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int odd = 0;
+		int even = 0;
+		while (true) {
+			int i = sc.nextInt();
+			if (i!=0 & i % 2 == 0) {
+				even++;
+				
+			} else if (i!=0 & i % 2 == 1) {
+				odd++;
+			} else if (i==0){
+				System.out.println("odd : " + odd + "\neven : " + even);
+				break;
+			}
+
+		}
+
+	}
+
+}
