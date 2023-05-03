cdf93d19bfe922158982c8d9fc039ee982b30be2
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-03 14:52:03 +0900

    basic109

---
+++ jqjqjqjqjq/src/q109/Main.java
@@ -0,0 +1,17 @@
+package q109;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		int i2 = sc.nextInt();
+		int i3 = sc.nextInt();
+		
+		System.out.println("sum = "+(i+i2+i3));
+	}
+
+}
