d4a4e71974aaee9782275ba470ffc1406fffbcdf
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-10 09:39:49 +0900

    595

---
+++ jqjqjqjqjq/src/q595/Main.java
@@ -0,0 +1,14 @@
+package q595;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hong Gil Dong";
+		for(int i=3;i<=6;i++) {
+			System.out.print(str.charAt(i));
+		}
+		
+	}
+
+}
