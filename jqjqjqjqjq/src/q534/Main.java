05b3dd54eb60a4f5a6c23b4b0d388fa4778956a5
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-12 09:41:55 +0900

    534

--- jqjqjqjqjq/src/q534/Main.java
+++ jqjqjqjqjq/src/q534/Main.java
@@ -8,8 +8,10 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		char a = sc.next().charAt(0);
-		sc.close();
+		String b = Character.toString(a);
 		
+		sc.close();
+		//1.스위치
 		switch (a) {
 		case 'A': {
 			System.out.println("Excellent");
@@ -32,6 +34,21 @@ public class Main {
 			break;
 		}
 		default:
+			System.out.println("error");
+		}
+		
+		//2.if
+		if(b.equals("A")) {
+			System.out.println("Excellent");
+		}else if(b.equals("B")) {
+			System.out.println("Good");
+		}else if(b.equals("C")) {
+			System.out.println("Usually");
+		}else if(b.equals("D")) {
+			System.out.println("Effort");
+		}else if(b.equals("F")) {
+			System.out.println("Failure");
+		}else {
 			System.out.println("error");
 		}
 	
