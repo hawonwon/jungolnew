d654c8d2185e0787bd886cb76a6dab2ed14a30c3
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-12 10:05:06 +0900

    547

---
+++ jqjqjqjqjq/src/q547/Main.java
@@ -0,0 +1,12 @@
+package q547;
+
+public class Main {
+	public static void main(String[] args) {
+		for(int i=2;i<=6;i++) {
+			for(int j=i;j<i+5;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
