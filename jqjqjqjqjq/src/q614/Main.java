75f5c7f0db1a4dc7890e61c907c05e9a56659b91
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-15 17:06:42 +0900

    614 구조체

---
+++ jqjqjqjqjq/src/q614/Main.java
@@ -0,0 +1,38 @@
+package q614;
+
+import java.util.Scanner;
+
+class School {
+	String name;
+	int grade;
+	
+	public School() {
+		// TODO Auto-generated constructor stub
+		name = "Jejuelementary";
+		grade = 6;
+	}
+	
+	School(String s, int g) {
+		name = s;
+		grade = g;
+	}
+}
+
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		School s1 = new School();
+		String name = sc.next();
+		int grade = sc.nextInt();
+		
+		School s2 = new School(name, grade);
+		
+		System.out.println(s1.grade+" grade in "+s1.name+" School");
+		System.out.println(s2.grade +" grade in "+s2.name+" School");
+	}
+
+}
