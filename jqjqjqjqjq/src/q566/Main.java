08ff2eab517ac9209555d00b6376a0b602ac6213
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-15 14:22:50 +0900

    566 배열2

---
+++ jqjqjqjqjq/src/q566/Main.java
@@ -0,0 +1,30 @@
+package q566;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int[] ar = new int[100];
+		ar[0] = 100;
+		ar[1] = sc.nextInt();
+		int cnt = 2;
+		
+		
+		while(true) {
+			ar[cnt] = ar[cnt-2]-ar[cnt-1];
+			if(ar[cnt]<0) {
+				break;
+			}
+			cnt++;
+		}
+		for(int i=0;i<=cnt;i++) {
+			
+			
+			System.out.print(ar[i]+" ");
+		}
+	}
+
+}
