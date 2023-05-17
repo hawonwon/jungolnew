e13a872aab859aca07980ff10ac7e7864681f754
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-17 13:35:54 +0900

    560 최소값

--- jqjqjqjqjq/src/q560/Main.java
+++ jqjqjqjqjq/src/q560/Main.java
@@ -7,15 +7,23 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int[] nums = new int[10];
+//		int[] nums = new int[10];
 		int m = 1000;
+//		for(int i =0;i<nums.length;i++) {
+//			nums[i] = sc.nextInt();
+//			if(nums[i]<m)
+//			m = nums[i];
+//		}
+//		System.out.println(m);
+			
+		
 		for(int i =0;i<10;i++) {
-			nums[i] = sc.nextInt();
-			if(nums[i]<m)
-			m = nums[i];
+			int c = sc.nextInt();
+			if(c<m) {
+				m = c;
+			}
 		}
 		System.out.println(m);
-			
 	}
 
 }
