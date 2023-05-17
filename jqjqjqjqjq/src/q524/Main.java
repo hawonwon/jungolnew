d42a15047e2f74ea4580e96e6083f2e822b623fd
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-17 13:33:29 +0900

    524

--- jqjqjqjqjq/src/q524/Main.java
+++ jqjqjqjqjq/src/q524/Main.java
@@ -1,10 +1,20 @@
 package q524;
 
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i =  sc.nextInt();
+        int i2 =  sc.nextInt();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        boolean b = i!=0;
+        boolean b2 = i2!=0;
 
-	}
+        System.out.println(b&b2);
+        System.out.println(b|b2);
 
+
+    }
 }
