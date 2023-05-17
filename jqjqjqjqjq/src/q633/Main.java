6ed9b2a1ef410da4e8f13581996e224f9de60e63
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-17 13:32:51 +0900

    633

--- jqjqjqjqjq/src/q633/Main.java
+++ jqjqjqjqjq/src/q633/Main.java
@@ -1,10 +1,53 @@
 package q633;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        W: while (true){
+            System.out.println("1. Korea");
+            System.out.println("2. USA");
+            System.out.println("3. Japan");
+            System.out.println("4. China");
+            System.out.print("number? ");
+            int i = sc.nextInt();
+            System.out.println();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+            
+
+            switch (i){
+                case 1 :
+                  
+                    System.out.println("Seoul");
+                    System.out.println();
+                    break;
+                case 2 :
+                  
+                    System.out.println("Washington");
+                    System.out.println();
+                    break;
+                case 3 :
+                   
+                    System.out.println("Tokyo");
+                    System.out.println();
+                    break;
+                case 4 :
+                   
+                    System.out.println("Beijing");
+                    System.out.println();
+                    break;
+                default:
+                  
+                    System.out.println("none");
+                    System.out.println();
+                    break W;
+
+
+            }
+
+        }
+    }
 
 }
