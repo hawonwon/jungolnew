483dea90a82b048221eb9ff944340f944b8c26fc
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-22 13:50:17 +0900

    604

---
+++ jqjqjqjqjq/src/q604/Main.java
@@ -0,0 +1,29 @@
+package q604;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		String[] str = new String[10];
+		
+		for(int i=0;i<str.length;i++) {
+			str[i] = sc.next();
+			
+		}
+		char c = sc.next().charAt(0);
+		
+		System.out.println();
+		for(int i=0;i<str.length;i++) {
+			if(c==str[i].charAt(str[i].length()-1) ) {
+				System.out.println(str[i]);
+			}
+			
+		}
+	}
+
+}
