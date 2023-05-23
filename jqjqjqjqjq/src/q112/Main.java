61051a0b49894572a52021983a0414679c781673
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-23 09:35:55 +0900

    112

---
+++ jqjqjqjqjq/src/q112/Main.java
@@ -0,0 +1,17 @@
+package q112;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		
+		System.out.println(i+" / " +j+" = " +i/j+"..."+i%j);
+	}
+
+}
