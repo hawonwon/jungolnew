568ded6ec8c38d4a4ef4025cb3bdc831ead825cc
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-24 09:37:19 +0900

    571 구조체

---
+++ jqjqjqjqjq/src/q571/Main.java
@@ -0,0 +1,29 @@
+package q571;
+
+import java.util.Scanner;
+
+class St {
+	
+	
+	void run(int x){
+		for(int i=0;i<x;i++) {
+			System.out.println("~!@#$^&*()_+|");
+		}
+		
+		
+	}
+}
+
+public class Main {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		St s1 = new St();
+		s1.run(a);
+	}
+
+}
