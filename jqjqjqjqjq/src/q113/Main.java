4ca0563e5bbd043f5117264e192f4b3ae8b99975
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-24 09:34:29 +0900

    113

---
+++ jqjqjqjqjq/src/q113/Main.java
@@ -0,0 +1,20 @@
+package q113;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int w = sc.nextInt()+5;
+		int h = sc.nextInt()*2;
+		System.out.println("width = "+w);
+		System.out.println("length = "+h);
+		System.out.println("area = " +w*h);
+		
+		
+	}
+
+}
