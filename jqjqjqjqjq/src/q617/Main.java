082a68a1f6da5b8fec2e47b981f8487957896af3
Class01 <Class01@DESKTOP-V50GOLG>
2023-05-30 13:57:34 +0900

    617 구조체배열작은사람

--- jqjqjqjqjq/src/q617/Main.java
+++ jqjqjqjqjq/src/q617/Main.java
@@ -2,39 +2,53 @@ package q617;
 
 import java.util.Scanner;
 
-class Person {
-	
-	String name;
-	int height;
-	
-	
-	Person(String a, int b){
-		height = Integer.MAX_VALUE;
-		
-		if (b < height) {
-			height = b;
-			name = a;
-		}
-	}
+class Short {
+    String name;
+    int height;
+
+    void person(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+    
+    void plint() {
+    	
+    	System.out.println(this.name+" "+this.height);
+    }
+    
+    
+
 }
 
-
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String[] s = new String[5];
-		int[] h = new int[5];
-		
-		for(int j=0;j<5;j++) {
-			s[j] = sc.next();
-			h[j] = sc.nextInt();
-			
-		}
-		
-		
-		
-	}
+        Short[] s1 = new Short[5];
+        Short ss = new Short();
+        ss.person("x", Integer.MAX_VALUE);
+        
+        
+//        int x = Integer.MAX_VALUE;
+//        int y = 0;
+        
+        for(int i=0;i<5;i++){
+
+                s1[i] = new Short();
+                String n = sc.next();
+                int h = sc.nextInt();
+                 s1[i].person(n, h);
+            if(s1[i].height<ss.height){
+                ss.person(s1[i].name,s1[i].height);
+                
+            }
+        }
+        ss.plint();
+//        System.out.println(s1[y].name+" "+s1[y].height);
+        
+
+    }
+
+
 
 }
